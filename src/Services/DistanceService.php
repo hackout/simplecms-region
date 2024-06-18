17748febfe5def89ed9994f5cc7306b659073085
hackout <hackout@vip.qq.com>
2024-06-18 11:10:45 +0800

    修复服务

--- src/Services/DistanceService.php
+++ src/Services/DistanceService.php
@@ -10,39 +10,41 @@ class DistanceService
      * 增加距离Select
      *
      * @author Dennis Lui <hackout@vip.qq.com>
+     * @param  SimpleService $service
      * @param  float         $lat
      * @param  float         $lng
      * @param  string        $column
      * @return SimpleService
      */
-    public function selectDistance(float $lat, float $lng,string $column = 'location'):SimpleService
+    public function selectDistance(SimpleService $service, float $lat, float $lng,string $column = 'location'):SimpleService
     {
         $distanceRaw = "ST_Distance_Sphere($column, POINT($lng, $lat)) AS distance";
-        $this->setSelectRaw($distanceRaw);
-        return $this;
+        $service->setSelectRaw($distanceRaw);
+        return $service;
     }
 
     /**
      * 按距离查询
      *
      * @author Dennis Lui <hackout@vip.qq.com>
+     * @param  SimpleService $service
      * @param  float         $lat
      * @param  float         $lng
      * @param  integer       $maxDistance
      * @param  string        $column
      * @return SimpleService
      */
-    public function queryDistance(float $lat, float $lng, float $maxDistance = 50,string $column = 'location'):SimpleService
+    public function queryDistance(SimpleService $service, float $lat, float $lng, float $maxDistance = 50,string $column = 'location'):SimpleService
     {
         $distanceRaw = "ST_Distance_Sphere($column, POINT($lng, $lat)) AS distance";
-        $this->setSelectRaw($distanceRaw);
-        $this->appendQuery([
+        $service->setSelectRaw($distanceRaw);
+        $service->appendQuery([
             [
                 function ($query) use ($distanceRaw, $maxDistance) {
                     $query->whereRaw("$distanceRaw <= $maxDistance");
                 }
             ]
         ]);
-        return $this;
+        return $service;
     }
 }
